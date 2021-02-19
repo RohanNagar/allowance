d5e20bce2a730dfe756d030b044094633eaef1fe
Rohan Nagar <rohan.nagar@utexas.edu>
2021-02-19 14:38:54 -0800

    Add xcode project

---
+++ Allowance/AllowanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllowanceApp.swift
+//  Allowance
+//
+//  Created by Rohan Nagar on 2/19/21.
+//
+
+import SwiftUI
+
+@main
+struct AllowanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
